4b176ab6f60889c0117f69ac2a8b996524e4b348
clctrulala <clctrulala@gmail.com>
2022-04-25 12:48:28 +0300

    realize transaction

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -6,6 +6,7 @@ import jm.task.core.jdbc.util.Util;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class UserDaoJDBCImpl implements UserDao {
@@ -16,23 +17,40 @@ public class UserDaoJDBCImpl implements UserDao {
     public UserDaoJDBCImpl() {
     }
 
-    private void sqlUpdate(String request) {
+    private void sqlUpdate(String request) throws SQLException {
         try ( Connection connection = util.getDBConnect() ) {
             try ( Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE) ) {
-                statement.executeUpdate(request);
+                try {
+                    connection.setAutoCommit(false);
+                    statement.executeUpdate(request);
+                    connection.commit();
+                } catch (SQLException sqlErr) {
+                    LOGGER.severe("Error in COMMIT operation of the table.");
+                    try {
+                        connection.rollback();
+                    } catch (SQLException rollbackErr) {
+                        LOGGER.log(Level.SEVERE, "ROLLBACK error.", rollbackErr);
+                    }
+                }
             }
-        } catch (SQLException sqlErr) {
-            LOGGER.severe("Error in UPDATE of the table.");
         }
     }
 
     public void createUsersTable() {
-        sqlUpdate( String.format("CREATE TABLE IF NOT EXISTS %s (id BIGINT PRIMARY KEY AUTO_INCREMENT," +
-                                 " Name VARCHAR(20), lastName VARCHAR(20), age TINYINT);", dbTableName) );
+        try {
+            sqlUpdate( String.format("CREATE TABLE IF NOT EXISTS %s (id BIGINT PRIMARY KEY AUTO_INCREMENT," +
+                    " Name VARCHAR(20), lastName VARCHAR(20), age TINYINT);", dbTableName) );
+        } catch (SQLException sqlErr) {
+            LOGGER.severe("Table not created.");
+        }
     }
 
     public void dropUsersTable() {
-        sqlUpdate( String.format("DROP TABLE IF EXISTS %s;", dbTableName) );
+        try {
+            sqlUpdate( String.format("DROP TABLE IF EXISTS %s;", dbTableName) );
+        } catch (SQLException sqlErr) {
+            LOGGER.severe("Table not dropped.");
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
@@ -40,18 +58,33 @@ public class UserDaoJDBCImpl implements UserDao {
 
         try ( Connection connection = util.getDBConnect() ) {
             try ( PreparedStatement statement = connection.prepareStatement(request) ) {
-                statement.setNString(1, name);
-                statement.setNString(2, lastName);
-                statement.setByte(3, age);
-                statement.executeUpdate();
+                try {
+                    connection.setAutoCommit(false);
+                    statement.setNString(1, name);
+                    statement.setNString(2, lastName);
+                    statement.setByte(3, age);
+                    statement.executeUpdate();
+                    connection.commit();
+                } catch (SQLException sqlErr) {
+                    LOGGER.severe("Error in COMMIT operation of the table.");
+                    try {
+                        connection.rollback();
+                    } catch (SQLException rollbackErr) {
+                        LOGGER.log(Level.SEVERE, "ROLLBACK error.", rollbackErr);
+                    }
+                }
             }
         } catch (SQLException sqlErr) {
-            LOGGER.severe("Error in UPDATE of the table.");
+            LOGGER.severe("User not saved.");
         }
     }
 
     public void removeUserById(long id) {
-        sqlUpdate( String.format("DELETE FROM %s WHERE id=%d;", dbTableName, id) );
+        try {
+            sqlUpdate( String.format("DELETE FROM %s WHERE id=%d;", dbTableName, id) );
+        } catch (SQLException sqlErr) {
+            LOGGER.severe("User not removed.");
+        }
     }
 
     public List<User> getAllUsers() {
@@ -61,23 +94,25 @@ public class UserDaoJDBCImpl implements UserDao {
             try ( Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE) ) {
                 ResultSet resultSet = statement.executeQuery(String.format("SELECT * FROM %s;", dbTableName));
 
-                if ( resultSet.first() ) {
-                    do {
-                        User user = new User( resultSet.getString("name"), resultSet.getString("lastName"), resultSet.getByte("age") );
+                while ( resultSet.next() ) {
+                    User user = new User( resultSet.getString("name"), resultSet.getString("lastName"), resultSet.getByte("age") );
 
-                        user.setId(resultSet.getLong("id"));
-                        allUsers.add(user);
-                    } while (resultSet.next());
+                    user.setId(resultSet.getLong("id"));
+                    allUsers.add(user);
                 }
             }
         } catch (SQLException sqlErr) {
             LOGGER.severe("Error in users QUERY.");
         }
-
         return allUsers;
     }
 
     public void cleanUsersTable() {
-        sqlUpdate( String.format("DELETE FROM %s;", dbTableName) );
+        try  {
+            sqlUpdate( String.format("DELETE FROM %s;", dbTableName) );
+        } catch (SQLException sqlErr) {
+            LOGGER.severe("Table not cleaned.");
+        }
     }
+
 }
